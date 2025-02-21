8cdcab1e591d8063dca4df19af13c4edbf867769
Abraham Galue <abrahamgalue512@gmail.com>
2025-02-21 15:15:54 -0400

    Disabled submit btn on initial load

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ window.onload = () => {
     .then(res => res.json())
     .then(data => {
       const mealsList = document.getElementById('meals-list')
+      const submit = document.getElementById('submit')
       const template = data.map(x => '<li>' + x.name + '</li>').join('')
       mealsList.innerHTML = template
+      submit.removeAttribute('disabled')
     })
 }
